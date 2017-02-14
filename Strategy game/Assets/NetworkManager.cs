ab4626c205e46fede7bfb3672dffbd3ac69bc242
CRISTIANSOBREME\Cristian Main PC <crisw8mc@gmail.com>
2017-02-14 12:55:46 +0100

    tinyterrain out, Network changes

--- Strategy game/Assets/NetworkManager.cs
+++ Strategy game/Assets/NetworkManager.cs
@@ -6,7 +6,7 @@ public class NetworkManager : MonoBehaviour {
 
 	private const string typeName = "EstrategiaiGenial2017";
 	private const string gameName = "Habitacion";
-	private HostData[] hostlist ();
+	private HostData[] hostlist;
 	private void RefreshHostList()
 	{
 		MasterServer.RequestHostList (typeName);
@@ -41,16 +41,31 @@ public class NetworkManager : MonoBehaviour {
 		Debug.Log ("Server Initializied");
 	}
 
-	void OnGUI()
-	{
-		if (!Network.isClient && !Network.isServer) 
-		{
-			if (GUI.Button (new Rect (100, 100, 250, 100), "Start Server")) 
-			{
-				
-				StartServer ();
-			}
-		}
-	}
+    void OnGUI()
+    {
+        if (!Network.isClient && !Network.isServer)
+        {
+            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+            {
+
+                StartServer();
+            }
+
+            if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+            {
+                RefreshHostList();
+            }
+            if (hostlist != null)
+            {
+                for (int i = 0; i < hostlist.Length; i++)
+                {
+                    if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostlist[i].gameName))
+                    {
+                        JoinServer(hostlist[i]);
+                    }
+                }
+            }
+        }
+    }
 
 }
