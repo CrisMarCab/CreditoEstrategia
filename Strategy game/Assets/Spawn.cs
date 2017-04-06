78fd872090d13346887fe37d4efea98b606e6f24
DESKTOP-JJ1A081\Cristian <crisw8mc@gmail.com>
2017-04-06 18:49:59 +0200

    nuevoSpawn
    Spawnea enemigos en los spawnpoints

---
+++ Strategy game/Assets/Spawn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class Spawn : NetworkBehaviour
+{
+
+    public GameObject enemigo;
+    int objectsToSpawn;
+
+    public GameObject[] posicionesSpawn;
+
+    public float SpawnInterval = 15;
+    public float MaxObjectsSpawned = 8;
+    public float MinObjectsSpawned = 3;
+
+
+    private float _NextSpawn;
+
+
+
+    void Start()
+    {
+
+        _NextSpawn = Time.time + SpawnInterval;
+
+         objectsToSpawn = (int)Random.Range(MinObjectsSpawned, MaxObjectsSpawned);
+    }
+
+
+    void Update()
+    {
+        if (Time.time >= _NextSpawn && objectsToSpawn < MaxObjectsSpawned)
+        {
+            objectsToSpawn = objectsToSpawn + 1;
+
+            Instantiate(enemigo,posicionesSpawn[Random.Range(0,5)].transform.position, Quaternion.identity);
+
+            _NextSpawn = Time.time + SpawnInterval;
+        }
+    }
+}
