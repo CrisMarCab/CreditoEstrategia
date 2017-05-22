455f769a34759979d2ea98c909c3591fa9abc400
CrisMarCab <crisw8mc@gmail.com>
2017-05-22 13:09:20 +0200

    Vida y animaciones

--- Strategy game/Assets/Scripts/moveryatacar.cs
+++ Strategy game/Assets/Scripts/moveryatacar.cs
@@ -18,6 +18,10 @@ namespace CompleteProject
         private bool walking;
         private bool enemyClicked;
         private float nextFire;
+        private float cooldowntimer, cooldown;
+        vida vidaavion;
+        bool daño;
+
 
         // Use this for initialization
         void Awake()
@@ -25,6 +29,10 @@ namespace CompleteProject
             anim = GetComponent<Animator>();
 
             navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+            cooldowntimer = 1;
+            cooldown = cooldowntimer;
+
         }
 
         void Start()
@@ -48,6 +56,8 @@ namespace CompleteProject
                     if (hit.collider.CompareTag("Enemy"))
                     {
                         targetedEnemy = hit.transform;
+                        vidaavion = targetedEnemy.GetComponent<vida>();
+
                         enemyClicked = true;
                         Debug.Log("Enemigo clickado");
                     }
@@ -68,10 +78,22 @@ namespace CompleteProject
                 }
             }
 
+            if (daño)
+            {
+                if (cooldown < 0)
+                {
+                    cooldown = cooldowntimer;
+                    vidaavion.setVida_total(5);
+                }
+                else
+                {
+                    cooldown -= Time.deltaTime;
+                }
+            }
+
             if (enemyClicked)
             {
                 MoveAndShoot();
-
 
             }
 
@@ -87,14 +109,18 @@ namespace CompleteProject
             {
                 walking = true;
             }
-
         }
+
+
+
+
 
 
         private void MoveAndShoot()
         {
             if (targetedEnemy == null)
             {
+                daño = false;
                 return;
             }
 
@@ -102,9 +128,12 @@ namespace CompleteProject
 
             if (navMeshAgent.remainingDistance >= shootDistance)
             {
-
+                daño = false;
                 navMeshAgent.Resume();
                 walking = true;
+                anim.SetInteger("Disparar", 0);
+                anim.SetInteger("estadoAnimacion", 1);
+
             }
 
             if ((navMeshAgent.remainingDistance + 6) < shootDistance)
@@ -112,6 +141,7 @@ namespace CompleteProject
                 anim.SetInteger("estadoAnimacion", 0);
 
                 anim.SetInteger("Disparar", 1);
+                daño = true;
 
                 transform.LookAt(targetedEnemy);
                 Vector3 dirToShoot = targetedEnemy.transform.position - transform.position;
--- Strategy game/Assets/Scripts/vida.cs
+++ Strategy game/Assets/Scripts/vida.cs
@@ -2,27 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class vida : MonoBehaviour {
+public class vida : MonoBehaviour
+{
     [SerializeField]
     int vida_total = 100;
+    int vidadañovisual = 80;
+    TextMesh vidavisual;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void awake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //for (int i = 0; i <= gameObject.transform.childCount; i++)
+        foreach (Transform child in transform)
+        {
+            if (child.transform.name == "Vida")
+            {
+                vidavisual = child.GetComponent<TextMesh>();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     [SerializeField]
-    public int getVida_total() {
+    public int getVida_total()
+    {
         return vida_total;
     }
-    public void setVida_total(int daño) {
+    public void setVida_total(int daño)
+    {
         vida_total -= daño;
+
+        if (vida_total == vidadañovisual)
+        {
+            vidavisual.text = vidavisual.text.Remove(0, 1);
+            vidadañovisual -= 20;
+        }
     }
+
+
 
 
 }
