02091c44dac68e7968a22aca00e948954e2d0f5f
OsCerGar <oscergar@gmail.com>
2017-05-30 20:08:42 +0200

    Spawn de enemigos

--- Strategy game/Assets/Scripts/NetworkManager_spawn.cs
+++ Strategy game/Assets/Scripts/NetworkManager_spawn.cs
@@ -4,7 +4,17 @@ using UnityEngine.Networking;
 using UnityEngine;
 
 public class NetworkManager_spawn : NetworkManager {
+	public GameObject enemigo;
+	int objectsToSpawn;
 
+	public GameObject[] posicionesSpawn;
+
+	public float SpawnInterval = 15;
+	public float MaxObjectsSpawned = 8;
+	public float MinObjectsSpawned = 3;
+
+
+	private float _NextSpawn;
     public override void OnStartClient(NetworkClient client)
     {
         
@@ -18,7 +28,26 @@ public class NetworkManager_spawn : NetworkManager {
 
     }
 
+	void Start()
+	{
 
+		_NextSpawn = Time.time + SpawnInterval;
+
+		objectsToSpawn = (int)Random.Range(MinObjectsSpawned, MaxObjectsSpawned);
+	}
+
+
+	void Update()
+	{
+		if (Time.time >= _NextSpawn && objectsToSpawn < MaxObjectsSpawned)
+		{
+			objectsToSpawn = objectsToSpawn + 1;
+
+			Instantiate(enemigo,posicionesSpawn[Random.Range(0,5)].transform.position, Quaternion.identity);
+
+			_NextSpawn = Time.time + SpawnInterval;
+		}
+	}
 
 
     }
